5a0e1f871be58e8bbb416e12610b54e951827f67
SamCay <samuelo3@knights.ucf.edu>
2015-03-30 16:50:28 -0400

    Updated Tasks manager, method getActivityManager
    more additions, needs revision by luke, with how they are called.

--- Groop/src/social/TaskManager.java
+++ Groop/src/social/TaskManager.java
@@ -3,21 +3,41 @@ package social;
 import java.util.List;
 
 public class TaskManager{
-	private Task task;
-	private ActivityManager activity;
+	private ArrayList<Task> tasks;
+	private ActivityManager activityManager;
+	
+	// constructor
+	public TaskManager(){
+		this.tasks = new ArrayList<Task>();
+		this.activity = new ActivityManager;
+	}
 	
 	public Task createTask(String title, String description, Deadline deadline){
-		task = new Task();
+		Task task = new Task();
 		task.setTitle(title);
 		task.setDescription(description);
-		task.setDeadline( deadline);
+		task.setDeadline(deadline);
 		
 		return task;
+	}
+	
+	public void createTasks(int numTasks){
+		for(int i = 0; i < numTasks){
+			this.tasks.add(createTask("","",null));
+		}
+	}
+	
+	public ArrayList<Task> getTasks(){
+		return this.tasks
 	}
 	
 	public void deleteTask(Task task){
 		
 		task = null;
+	}
+	
+	public ActivityManager getAvtivityManager() {
+		return activityManager;
 	}
 	
 }
