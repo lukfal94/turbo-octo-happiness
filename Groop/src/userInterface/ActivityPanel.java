aaecd3df3a6d3cd3705eebdcc6ba13b1603fbbe2
Relados <blagh0@yahoo.com>
2015-04-17 01:25:54 -0400

    Add most functionality to ActivityPanel
    A couple of notes:
    - This is right now using an editActivity() function in the Registrar class, which does not currently exist.  That will have to be created.
    - I have not yet seen how this looks, as I can't get the program to run correctly.  I can almost guarantee there are size/spacing issues that will have to be resolved so it doesn't look horrendous.
    - For the list of Activities, I put the title of the task, followed by the username and then the timestamp.  This does not have to be final, and can be changed to whatever is found more suitable if needed.
    - I'm assuming that people should not be allowed to edit activities that aren't theirs, unless they are an admin.  I have also made the verify checkbox accessible to admins only.
    
    I coded a lot of this while sleep deprived (I wish I had an actual excuse for this, but it turns out I'm just an idiot at time management), so it's possible I looked over something incredibly stupid by mistake.  Eclipse didn't yell at me, so my fingers are crossed.
    
    Also, I'm sorry it took a couple days longer than I expected to push this, due to the previously mentioned time management thing and also because I had to sync it with the changes made while I was working locally.

--- Groop/src/userInterface/ActivityPanel.java
+++ Groop/src/userInterface/ActivityPanel.java
@@ -10,20 +10,32 @@ import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.DefaultListModel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import javax.swing.JOptionPane;
 
 import managers.SessionManager;
+import social.User;
 import util.Activity;
 import util.Task;
 import util.Task.TaskPriority;
@@ -33,8 +45,12 @@ import databaseComm.ServerResponse;
 public class ActivityPanel extends JPanel{
 	SessionManager sessionManager;
 	
-	JLabel testLabel;
-	JButton createActivityButton;
+	private JLabel testLabel;
+	private JButton createActivityButton;
+	private JButton editActivityButton;
+	private JList<String> activityList;
+	private JScrollPane activityListView;
+	private DefaultListModel<String> listModel;
 	
 	public ActivityPanel(SessionManager sm) {
 		testLabel = new JLabel("Activity Panel");
@@ -43,9 +59,27 @@ public class ActivityPanel extends JPanel{
 		createActivityButton = new JButton("New");
 		createActivityButton.addActionListener(new ActivityWindowButtonClick());
 		
+		editActivityButton = new JButton("Edit");
+		editActivityButton.addActionListener(new ActivityWindowButtonClick());
+		
+		listModel = new DefaultListModel<String>();
+		DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+		for (Activity a : sessionManager.getActiveGroup().getActivityManager().getActivityLog()) {
+			
+			listModel.addElement(a.getTask().getTitle() + " - " + a.getUser().getFullname() + ": " + dateFormat.format(a.getTimeStamp()));
+		}
+		
+		activityList = new JList<String>(listModel);
+		activityList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		activityListView = new JScrollPane(activityList);
+		
 		this.add(testLabel);
+		this.add(activityListView);
 		this.add(createActivityButton);
-		this.setBackground(Color.GREEN);
+		this.add(editActivityButton);
+		//this.setBackground(Color.GREEN);
+		// i'm assuming we don't want to keep it bright green
 	}
 	
 	public enum ActivityWindowMode {
@@ -55,10 +89,22 @@ public class ActivityPanel extends JPanel{
 	private class ActivityWindowButtonClick implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource().equals(createActivityButton))
-				openActivityWindow(ActivityWindowMode.NEW_ACTIVITY);
-//			else if(e.getSource().equals(editTaskButton))
-//				openTaskWindow(TaskWindowMode.EDIT_TASK);
+			if (activityList.getSelectedIndex() == -1)
+				JOptionPane.showMessageDialog(null, "Please select an activity to edit.", "No Selection", JOptionPane.INFORMATION_MESSAGE);
+			else {
+				if(e.getSource().equals(createActivityButton))
+					openActivityWindow(ActivityWindowMode.NEW_ACTIVITY);
+				else if(e.getSource().equals(editActivityButton)) {
+					boolean isAdmin = false;
+					for (User u : sessionManager.getActiveGroup().getAdmins())
+						if (u.equals(sessionManager.getActiveUser()))
+							isAdmin = true;
+					if (isAdmin || sessionManager.getActiveGroup().getActivityManager().getActivityLog().get(activityList.getSelectedIndex()).getUser().equals(sessionManager.getActiveUser()))
+						openActivityWindow(ActivityWindowMode.EDIT_ACTIVITY);
+					else
+						JOptionPane.showMessageDialog(null, "You do not have permission to edit that activity.", "Forbidden", JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
 		}
 	}
 	
@@ -76,8 +122,10 @@ public class ActivityPanel extends JPanel{
 		private JLabel descriptionLabel;
 		
 		// Input Elements
-		private JComboBox taskComboBox;
+		private JComboBox<Task> taskComboBox;
 		private JTextArea descriptionTextArea;
+		private JSpinner timeSpentSpinner;
+		private JCheckBox verifyCheckBox;
 		
 		// Action Buttons
 		private JButton submitButton;
@@ -98,9 +146,10 @@ public class ActivityPanel extends JPanel{
 			// Init Elements Unique to Window Mode
 			switch(mode) {
 				case NEW_ACTIVITY:
-					initNewTaskWindow();
+					initNewActivityWindow();
 					break;
 				case EDIT_ACTIVITY:
+					initEditActivityWindow();
 					break;
 			}
 			
@@ -127,8 +176,9 @@ public class ActivityPanel extends JPanel{
 			
 			// Initialize field label
 			selectTaskLabel = new JLabel("Select a Task", SwingConstants.RIGHT);
-			timeSpentLabel =	new JLabel("Time Spend", SwingConstants.RIGHT);
+			timeSpentLabel = new JLabel("Time Spend", SwingConstants.RIGHT);
 			descriptionLabel = new JLabel("Description", SwingConstants.RIGHT);
+			verifyCheckBox = new JCheckBox("Verified");
 			
 			
 			// Init input fields
@@ -140,6 +190,9 @@ public class ActivityPanel extends JPanel{
 			
 			submitButton = new JButton("Submit");
 			submitButton.addActionListener(new SubmitButtonPress());
+			
+			cancelButton = new JButton("Cancel");
+			cancelButton.addActionListener(new CancelButtonPress());
 			
 			descriptionTextArea.setLineWrap(true);
 			descriptionTextArea.setWrapStyleWord(true);
@@ -162,16 +215,40 @@ public class ActivityPanel extends JPanel{
 			this.addComponent(3, 0, 5, 1, gbC, activityWindowPanel, taskComboBox);
 			this.addComponent(0, 1, 3, 1, gbC, activityWindowPanel, descriptionLabel);
 			this.addComponent(3, 1, 6, 2, gbC, activityWindowPanel, descriptionTextArea);
-			this.addComponent(2, 2, 3, 1, gbC, activityWindowPanel, submitButton);
+			this.addComponent(0, 2, 3, 1, gbC, activityWindowPanel, timeSpentLabel);
+			this.addComponent(3, 2, 5, 1, gbC, activityWindowPanel, timeSpentSpinner);
+			this.addComponent(0, 4, 2, 1, gbC, activityWindowPanel, cancelButton);
+			this.addComponent(2, 4, 3, 1, gbC, activityWindowPanel, submitButton);
+			
+			boolean isAdmin = false;
+			for (User u : sessionManager.getActiveGroup().getAdmins())
+				if (u.equals(sessionManager.getActiveUser()))
+					isAdmin = true;
+			
+			if (isAdmin)
+				this.addComponent(0, 3, 3, 1, gbC, activityWindowPanel, verifyCheckBox);
 			
 			this.add(activityWindowPanel, BorderLayout.CENTER);
 		}
 		
 		// Initializes the components for the "New Activity" window
-		private void initNewTaskWindow() {
+		private void initNewActivityWindow() {
 
 			this.setTitle("Create a New Activity");
 		 
+		}
+		
+		private void initEditActivityWindow() {
+			
+			this.setTitle("Edit an Activity");
+			
+			Activity act = sessionManager.getActiveGroup().getActivityManager().getActivityLog().get(activityList.getSelectedIndex());
+			
+			taskComboBox.setSelectedItem(act.getTask());
+			descriptionTextArea.setText(act.getDescription());
+			timeSpentSpinner.setValue(act.getTimeSpend());
+			verifyCheckBox.setSelected(act.isVerified());
+			
 		}
 
 		
@@ -196,7 +273,11 @@ public class ActivityPanel extends JPanel{
 
 				// Send the new activity to the DB.
 				try {
-					response = registrar.addNewActivity();
+					if (currMode == ActivityWindowMode.NEW_ACTIVITY)
+						response = registrar.addNewActivity();
+					else
+						// TODO: add editActivity function to registrar
+						response = registrar.editActivity();
 				} catch(Exception ex) {
 					System.out.println(ex);
 				}
@@ -205,11 +286,34 @@ public class ActivityPanel extends JPanel{
 					ServerResponse servResponse = (ServerResponse)response;
 					// TODO Error handling
 				} else if(response.getClass().equals(Activity.class)) {
-					Activity newActivity = (Activity)response;
-					sm.getActiveGroup().getActivityManager().addActivity(newActivity);
+					if (currMode == ActivityWindowMode.NEW_ACTIVITY) {
+						Activity newActivity = (Activity) response;
+						newActivity.setTimeStamp(new Date());
+						DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+						sm.getActiveGroup().getActivityManager().addActivity(newActivity);
+						listModel.addElement(newActivity.getTask().getTitle() + " - " + newActivity.getUser().getFullname() + ": " + dateFormat.format(newActivity.getTimeStamp()));
+				
+					}
+					else {
+						Activity editActivity = (Activity) response;
+						editActivity.setTimeStamp(new Date());
+						DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+						sm.getActiveGroup().getActivityManager().getActivityLog().set(editActivity.getId(), editActivity);
+						listModel.set(editActivity.getId(), editActivity.getTask().getTitle() + " - " + editActivity.getUser().getFullname() + ": " + dateFormat.format(editActivity.getTimeStamp()));
+					}
 				}
-	
 			}
+		}
+		
+		private class CancelButtonPress implements ActionListener {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				close();
+			}
+		}
+		
+		private void close() {
+			this.dispose();
 		}
 	}
 }
